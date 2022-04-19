48e28b14bfed56335ffe4e10c528c38f5da9d23d
LazyShagyDude <59170465+LazyShagyDude@users.noreply.github.com>
2022-04-19 20:56:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 function App() {
   return (
     <div className="wrapper clear">
-      <div className="overlay">
+      <div style={{display: 'none'}} className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
-          <div className="cartItem">
-            <img src="/img/books/book1.png"></img>
-            <div>
-              <p>Мастер… без Маргариты</p>
+          <h2 className="mb-15 d-flex justify-between">Корзина 
+          <img className="removeBtn cu-p" src="/img/delete.svg"></img>
+          </h2>
+         <div className="items">
+         <div className="cartItem d-flex align-center mb-20">
+            <img className="mr-20" src="/img/books/book1.png"></img>
+            <div className="mr-20">
+              <p className="mb-5">Мастер… без Маргариты</p>
               <b>636 руб</b>
             </div>
+            <img className="removeBtn" src="/img/delete.svg"></img>
           </div>
+         </div>
+         <ul className="cartTotalBlock">
+           <li className="d-flex">
+             <span>Итого:</span>
+             <div></div>
+             <b>0 руб.</b>
+           </li>
+         </ul>
+         <button className="order">Оформить заказ</button>
         </div>
       </div>
       <header className="d-flex justify-between align-center p-40">
